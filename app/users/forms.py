fd7bff86c2d789055a1fc6510ac75fb87b32c2d1
lilitstepanyan19 <lil-lid@mail.ru>
2025-06-14 23:22:11 +0400

    add registration

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,8 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+    # user = forms.CharField()
+    # password = forms.CharField()
+
     class Meta:
         model = User
+        fields = ['username', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        ]
         
--- app/users/views.py
+++ app/users/views.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
     if request.method == 'POST':
@@ -26,8 +26,19 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title' : 'Home - Registration'
+        'title' : 'Home - Registration',
+        'form' : form
     }
     return render(request, 'users/registration.html', context)
 
