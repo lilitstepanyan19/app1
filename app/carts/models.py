8813ef3c53f862b886ed992c13a5563ae629bda2
lilitstepanyan19 <lil-lid@mail.ru>
2025-06-16 23:26:22 +0400

    added carts

---
+++ app/carts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from goods.models import Product
+
+from users.models import User
+
+
+class ClassQuerySet(models.QuerySet):
+    def total_price(self):
+        return sum(cart.product_prise() for cart in self)
+
+    def total_price(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+class Cart(models.Model):
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, verbose_name = 'Օգտատեր')
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name='Պրոդուկտ')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Quantity')
+    session_key = models.CharField(max_length=32, blank=True, null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Date of added')
+        
+    class Meta:
+        db_table = 'cart'
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
+
+    objects = ClassQuerySet().as_manager()
+
+    def product_prise(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f'cart {self.user.username} | Product {self.product.name} | {self.quantity}'
--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,9 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 def login(request):
     if request.method == 'POST':
@@ -13,6 +14,11 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f'Dear {username}, you login in acount')
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -32,6 +38,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f'Dear {user.username}, you are registration successfully')
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -43,14 +50,30 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title' : 'Home - Profile'
+        'title' : 'Home - Profile',
+        'form' : form
     }
     return render(request, 'users/profile.html', context)
 
 
+def users_cart(request):
+    return render(request, 'users/users_cart.html')
 
+
+@login_required
 def logout(request):
-    ...
+    messages.success(request, f'Dear {request.user.username}, you are LOGOUT')
+    auth.logout(request)
+    return redirect(reverse('main:index'))
