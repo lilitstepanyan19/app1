66d77e1490bc77f6ebc66740ef00a70c913ec39e
lilitstepanyan19 <lil-lid@mail.ru>
2025-06-18 23:11:35 +0400

    finish carts  views

--- app/users/views.py
+++ app/users/views.py
@@ -16,7 +16,9 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f'Dear {username}, you login in acount')
 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
 
                 return HttpResponseRedirect(reverse('main:index'))
