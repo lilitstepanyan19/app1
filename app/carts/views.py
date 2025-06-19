c3d37cd767c8506e62fa86c1e6215e26101c6630
lilitstepanyan19 <lil-lid@mail.ru>
2025-06-19 22:16:38 +0400

    jquary don't work

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,10 +1,16 @@
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.shortcuts import redirect, render
 from goods.models import Product
 from carts.models import Cart
+from carts.utils import get_user_cart
 
 
-def cart_add(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+def cart_add(request): 
+
+    product_id = request.POST.get('product_id')
+
+    product = Product.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -15,7 +21,18 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
+    
+    user_cart = get_user_cart(request)
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart.html', {'carts' : user_cart}, request=request)
+    
+    response_data = {
+        'message' : 'Product added in cart!',
+        'cart_items_html' : cart_items_html
+    }
+
+    return JsonResponse(request)
+
 
 
 def cart_change(request, product_slug):
