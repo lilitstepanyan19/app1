bb4eb3b4cd89d938c4c8db67ea2b59526228953d
lilitstepanyan19 <lil-lid@mail.ru>
2025-06-20 21:19:51 +0400

    remove cart ajax

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,19 +1,21 @@
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.shortcuts import redirect, render
+from django.urls import reverse
 from goods.models import Product
 from carts.models import Cart
 from carts.utils import get_user_cart
 
 
-def cart_add(request): 
-
-    product_id = request.POST.get('product_id')
+def cart_add(request):
+    
+    product_id = request.POST.get("product_id")
 
     product = Product.objects.get(id=product_id)
-
+    
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
+
         if carts.exists():
             cart = carts.first()
             if cart:
@@ -21,25 +23,91 @@ def cart_add(request):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1)
     
     user_cart = get_user_cart(request)
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts' : user_cart}, request=request)
-    
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
     response_data = {
-        'message' : 'Product added in cart!',
-        'cart_items_html' : cart_items_html
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
     }
 
-    return JsonResponse(request)
+    return JsonResponse(response_data)
+            
 
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
 
-
-def cart_change(request, product_slug):
-    ...
-
-
-def cart_remove(request, cart_id):
     cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    user_cart = get_user_cart(request)
+
+    context = {"carts": user_cart}
+
+    # if referer page is create_order add key orders: True to context
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["order"] = True
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", context, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
+
+
+
+def cart_remove(request):
+    
+    cart_id = request.POST.get("cart_id")
+
+    cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_cart(request)
+
+    # context = {"carts": user_cart}
+
+    # if referer page is create_order add key orders: True to context
+    # referer = request.META.get('HTTP_REFERER')
+    # if reverse('orders:create_order') in referer:
+    #     context["order"] = True
+
+    # cart_items_html = render_to_string(
+    #     "carts/includes/included_cart.html", context, request=request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
